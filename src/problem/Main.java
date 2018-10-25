6bac48a8474df5c92a29bca320c3dc69bb0ad9c2
camilve <camilve@stud.ntnu.no>
2018-10-26 07:34:49 +1000

    changed input file and english configuration

--- src/problem/Main.java
+++ src/problem/Main.java
@@ -1,14 +1,15 @@
 package problem;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
         ProblemSpec ps;
         try {
-            ps = new ProblemSpec("input1.txt");
+            ps = new ProblemSpec("examples\\level_1\\input1.txt");
             System.out.println(ps.toString());
         } catch (IOException e) {
             System.out.println("IO Exception occurred");
